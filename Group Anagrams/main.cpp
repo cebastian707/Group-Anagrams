5a231ce293be8c37ba0310bd08cdc570b09abf0b
Cebastian Santiago Hernandez <57859731+cebastian707@users.noreply.github.com>
2022-05-21 13:45:48 -0700

    Add project files.

---
+++ Group Anagrams/main.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+
+//helper function 
+std::vector<std::vector<std::string>> groupAnagramshelper(std::map<std::string,std::vector<std::string>> maps){
+	//variables
+	std::vector<std::vector<std::string>> anagrams;
+
+	for (auto& i : maps){
+		anagrams.push_back(i.second);
+
+	}
+
+	return anagrams;
+}
+
+
+
+
+std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+	//variables
+	std::map<std::string, std::vector<std::string>> hash;
+	std::string sort = "";
+	
+	//for loop to store the strings in the hash function
+	for (size_t i = 0; i < strs.size(); i++) {
+		//get the current string and sort it
+		sort = strs[i];
+		std::sort(sort.begin(), sort.end());
+
+
+		//check if weve havent seen this string
+		if (hash.find(sort) == hash.end()) {
+			hash[sort].push_back(strs[i]);
+		
+		}
+
+		//if weve already seen this string push it back in the
+		//same vector
+		else {
+			hash[sort].push_back(strs[i]);
+		}
+	
+	}
+
+	return groupAnagramshelper(hash);
+}
+
+int main() {
+	std::vector<std::string> anagrams = { "eat", "tea", "tan", "ate", "nat", "bat" };
+	std::vector<std::vector<std::string>> ana;
+	
+	ana = groupAnagrams(anagrams);
+
+	//cout what we got
+	for (size_t i = 0; i < ana.size(); i++) {
+		for (size_t j = 0; j < ana[i].size(); j++){
+			std::cout << ana[i][j] << std::endl;
+		}
+	}
+
+
+	return 0;
+}
